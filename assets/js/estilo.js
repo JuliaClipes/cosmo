539880d24d512cc31240842d0f1b2945bfe81697
JuliaClipes <juliaclipes@hotmail.com>
2019-08-20 17:34:09 -0300

    Alteração index

--- assets/js/estilo.js
+++ assets/js/estilo.js
@@ -16,10 +16,3 @@ function topFunction() {
 
 
 var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
