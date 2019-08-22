3bee2a457af89dbd683f71fbc28c380f0cc82548
JuliaClipes <juliaclipes@hotmail.com>
2019-08-22 12:16:01 -0300

    Atendimento

--- assets/js/estilo.js
+++ assets/js/estilo.js
@@ -16,3 +16,19 @@ function topFunction() {
 
 
 var modal = document.getElementById('id01');
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#botao-topo').fadeIn();
+        } else {
+            $('#botao-topo').fadeOut();
+        }
+    });
+
+    $('#botao-topo').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+});
