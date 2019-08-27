973886abcd87b9741f49bdc451adbaf5947e3cbf
JuliaClipes <juliaclipes@hotmail.com>
2019-08-27 18:56:45 -0300

    Atendimento 27/08

--- assets/js/estilo.js
+++ assets/js/estilo.js
@@ -14,6 +14,22 @@ function topFunction() {
     document.documentElement.scrollTop = 0;
 }
 
+window.addEventListener('scroll', function(e) {
+    //var t = 200;
+    //if(window.scroll(0, 200)){
+    //    console.log("gzuis");
+    //    document.getElementById('id02').style.display = 'block';
+    //}
+    console.log(document.body.scrollTop);
+    if (document.body.scrollTop == 200) {
+        console.log("asdas");
+        document.getElementById('id02').style.display = 'block';
+    } else {
+       document.getElementById('id02').style.display = 'none';
+    }
+  
+});
+
 
 
 var modal = document.getElementById('id01');
